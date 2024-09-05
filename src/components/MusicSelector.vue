cbd097f4b3cb33783b10cde1b705bbad217b530c
williammabotjaeng <williammabotjaeng@gmail.com>
2024-09-05 21:40:03 +0200

    put method, update music object, mock server

--- src/components/MusicSelector.vue
+++ src/components/MusicSelector.vue
@@ -1,16 +1,32 @@
 <script setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
 
 const genres = ref(['Amapiano', 'Pop', 'Hip Hop', 'Jazz']);
 
-const selectGenre = (genre) => {
+const router = useRouter();
+
+const selectGenre = async (genre) => {
   console.log(`Selected genre: ${genre}`);
-}
+  
+  try {
+    const response = await axios.put('/api/music', {
+      playMusic: true,
+      selectedGenre: genre,
+    });
+    
+    console.log('Response from API:', response.data);
+    router.push('friends');
+  } catch (error) {
+    console.error('Error updating music preferences:', error);
+  }
+};
 </script>
 
 <template>
   <div class="background-image p-4 h-full flex flex-col items-center justify-center">
-    <h3 class="text-white text-2xl mb-4 mt-8 text-center">Before we begin... Would you like some Music while you order your drinks</h3>
+    <h3 class="text-white text-2xl mb-4 mt-8 text-center">Before we begin... Would you like some Music while you order your drinks?</h3>
     <h2 class="text-white text-4xl mb-8 text-center">Select a Genre</h2>
     <div class="flex flex-col space-y-8">
       <button
