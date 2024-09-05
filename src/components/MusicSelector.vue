65029f9167592b3354cca79a7ccc87fa9dbb8020
williammabotjaeng <williammabotjaeng@gmail.com>
2024-09-05 22:12:57 +0200

    responsive number input, typo, hiphop, no space

--- src/components/MusicSelector.vue
+++ src/components/MusicSelector.vue
@@ -4,7 +4,7 @@ import { useRouter } from 'vue-router';
 import { useToast } from 'vue-toastification';
 import axios from 'axios';
 
-const genres = ref(['Amapiano', 'Pop', 'Hip Hop', 'Jazz']);
+const genres = ref(['Amapiano', 'Pop', 'HipHop', 'Jazz']);
 
 const router = useRouter();
 
