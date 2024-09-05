1fc03128fb5cd03f9700eadf493a6cfd7376f0e3
williammabotjaeng <williammabotjaeng@gmail.com>
2024-09-05 17:01:54 +0200

    TheWelcome, markup, Welcome message, call-to-action

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,5 +3,13 @@
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <div class="h-screen bg-gray-800 flex items-center justify-center">
+    <div class="absolute inset-0 bg-blue-500 opacity-75"></div>
+    <div class="relative z-10 text-center text-white">
+        <h1 class="text-4xl font-bold mb-4">Welcome to the OpenBar, Enjoy your time here</h1>
+        <a href="#" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Get Started
+        </a>
+    </div>
+  </div>
 </template>
