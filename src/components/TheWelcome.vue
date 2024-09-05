ac1b03ad694c50f884df7fed7e0724c7b95bea81
williammabotjaeng <williammabotjaeng@gmail.com>
2024-09-05 17:04:52 +0200

    tailwind bug, css

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,15 +1,15 @@
 <script setup>
-
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
   <div class="h-screen bg-gray-800 flex items-center justify-center">
     <div class="absolute inset-0 bg-blue-500 opacity-75"></div>
     <div class="relative z-10 text-center text-white">
-        <h1 class="text-4xl font-bold mb-4">Welcome to the OpenBar, Enjoy your time here</h1>
-        <a href="#" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <h1 class="text-2xl font-bold mb-4">Welcome to the OpenBar, Enjoy your time here.</h1>
+        <RouterLink to="/music" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Get Started
-        </a>
+        </RouterLink>
     </div>
   </div>
 </template>
