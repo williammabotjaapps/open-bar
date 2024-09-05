5afe4011579168c45c87aef8392312b71b0e9827
williammabotjaeng <williammabotjaeng@gmail.com>
2024-09-05 22:45:32 +0200

    del MusicTopBar from friends

--- src/views/FriendsView.vue
+++ src/views/FriendsView.vue
@@ -1,11 +1,9 @@
 <script setup>
 import FriendsSelector from '@/components/FriendsSelector.vue';
 import DrinkAlone from '@/components/DrinkAlone.vue';
-import MusicTopBar from '@/components/MusicTopBar.vue';
 </script>
 
 <template>
-  <MusicTopBar />
   <div class="flex h-screen">
     <div class="w-1/2">
       <FriendsSelector />
