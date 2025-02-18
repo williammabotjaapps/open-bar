41c01c3e96b986651c948bdbaa8c9b3ea4431d56
The App Spaza <info@theappspaza.click>
2025-02-18 12:31:02 +0200

    proxy ip host

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://18.212.101.79:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
