b7de056309348bc6789745f9e4594ad030ea405e
The App Spaza <info@theappspaza.click>
2025-02-18 13:05:24 +0200

    https, open bar demo url

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://18.212.101.79:5000',
+        target: 'https://open-bar-demo.appspaza.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
